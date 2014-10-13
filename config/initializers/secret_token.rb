6551854ce4183aa100f36c13567b6c1361b3d172
codegard1 <c.odegard@gmail.com>
2014-10-13 12:17:35 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imagal::Application.config.secret_key_base = '1e8847f21233d8bebd30bf9777628d762e6aba47ff14d269e4a3787a46a03bc2ce2edf1723b38f3f8aa1ffa3426ac449c50d00f80c5ca3cd7f3cd2f6b7c02429'
