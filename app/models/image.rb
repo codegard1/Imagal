3206932fbcb8ca97bf1ace8b3bfb2c21b566ae29
codegard1 <c.odegard@gmail.com>
2014-10-26 15:59:37 -0400

    fixed image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,49 +1,52 @@
 class Image < ActiveRecord::Base
-	file= f
-	@file = f
+  def file= (f)
+    @file = f
+  end
 
-	validate
+  def validate
 	# Empty 'name' field is not allowed
 	errors.add_on_empty 'name'
 
 	# @file should contain file uploaded by HTTP
 	# it is either StringIO object (if file was smaller than 10kb)
 	# or Tempfile (otherwise)
-	  @file.kin_of? StringIO  @file.kind_if? TempFile
-	  errors.add_to_base "No File Selected"
+	if not (@file.kind_of? StringIO or @file.kind_of? TempFile)
+	  errors.add_to_base("No File Selected")
 	  return
+	end
 
 	# We can't use StringIO data to call external programs
 	# So, if object is StringIO -- we have to create Tempfile
 	# Unfortunately, in Rails it is impossible to force all uploads to be Tempfiles
 	# [is that true? - Chris, oct 20]
-	  @file.kind_of? StringIO  
-	  #Yes, if @file is StringIO, create new Tempfile and copy everything to it
-	  @real_file = Tempfile."AEGALLERY"
-	  while @file.?
-	    @real_file.write @file
-	
+  	if @file.kind_of? StringIO  	  	  
+	#Yes, if @file is StringIO, create new Tempfile and copy everything to it
+	  @real_file = Tempfile.new("AEGALLERY")
+	  while not @file.eof?
+	    @real_file.write @file.read
+	  end
+	else
+	  # Most uploads will be Tempfiles
+	  @real_file = @file
+	end
 
-	 # Most uploads will be Tempfiles
-	 @real_file = @file
+	# Here is a call to ImageMagick tool identity    
+ 	# it prints to standard output    
+ 	# type, width and height of images    
+ 	# (this is specified by -format %m,%w,%h) 
+ 	identify = '#{IMAGE_MAGICK_PATH}/identify -format %m, %w, %h #{@real_file.path} 2>&1'
+ 	# Now identify is a sring like "JPEG,640,480"
+	 	
+	# We split this string to array
+ 	@jpeg_info = identify.split(',',3)
+ 	# convert width and height to integer and assign it to object fields
+ 	self.width = @jpeg_info.to_i
+ 	self.height = @jpeg_info.to_i
 
-
-	 # Here is a call to ImageMagick tool identity    
-	 # it prints to standard output    
-	 # type, width and height of images    
-	 # (this is specified by -format %m,%w,%h) 
-	 identify = '#{IMAGE_MAGICK=PATH}/identify -format %m, %w, %h #{@real_file.path} 2>&1
-
-	 # Now identify is a sring like "JPEG,640,480"
-
-	 # We split this string to array
-	 @jpeg_info = identify.split,
-	 # convert width and height to integer and assign it to object fields
-	 .width = @jpeg_info.
-	 .height = @jpeg_info.
-
-	 # Finally, checking if everything was fine with this image
-	 # if file was not valid JPEG -- something will fail here
-	  @jpeg_info ==  @jpeg_info ~= 'JPEG' .width <=  .height <=
-	   errors.add_to_base "Wrong image format (use only JPEG) or broken data"
-	   return
+ 	# Finally, checking if everything was fine with this image
+ 	# if file was not valid JPEG -- something will fail here
+  	if @jpeg_info == nil or @jpeg_info[0] != 'JPEG' or self.width <= 0 or self.height <= 0
+   	  errors.add_to_base("Wrong image format (use only JPEG) or broken data")
+   	  return
+	end
+  end
